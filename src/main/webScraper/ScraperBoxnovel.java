bbcbea96e55595c8c6fec6626d7023939f87c9e8
MathiasSchnack <mathiasschnack@gmail.com>
2020-07-10 16:44:34 +0200

    ad

--- src/main/webScraper/ScraperBoxnovel.java
+++ src/main/webScraper/ScraperBoxnovel.java
@@ -23,14 +23,15 @@ public class ScraperBoxnovel {
         this.createPDF = createPDF;
     }
 
-    public static final String[] websites = { "boxnovel.com", "boxnovel.net" };
+    public static final String[] websites = {"boxnovel.com", "boxnovel.net", "wuxiaworld.com"};
 
     public void getNovel() {
 
-    int page = 1;
-    int pageCheck = 2;
+        int page = 1;
+        int pageCheck = 2;
 
-String checkWebsite = "boxnovel.com";
+        String checkWebsite1 = "boxnovel.com";
+        String checkWebsite2 = "wuxiaworld.com";
 
         JFrame frame1 = new JFrame("Choose a website");
         String novelWebsite = (String) JOptionPane.showInputDialog(frame1,
@@ -43,7 +44,7 @@ String checkWebsite = "boxnovel.com";
 
         String novelName = JOptionPane.showInputDialog("Novel Name - Make sure to put '-' between each word: 'shuras-wrath'");
 
-        if (novelWebsite.equals(checkWebsite)) {
+        if (novelWebsite.equals(checkWebsite1)) {
 
 
             for (page = 1; page <= pageCheck; page++) {
@@ -67,55 +68,182 @@ String checkWebsite = "boxnovel.com";
 
 
                 Elements temp = doc.select("p");
+                Elements test = doc.getElementsByClass("nav-next");
 
                 int k = 1;
 
-                 if (temp.size() < 30) {
+                if (temp.size() < 30) {
 
                     try {
                         doc = Jsoup.connect("https://boxnovel.com/novel/" + novelName + "/chapter-" + page + "_" + k).userAgent("mozilla/17.0").get();
                         temp = doc.select("p");
-                    } catch (IOException e) {
+
+                        if (temp.size() < 30) {
+                            try {
+                                doc = Jsoup.connect("https://boxnovel.com/novel/" + novelName + "/chapter-" + page + "-" + "end").userAgent("mozilla/17.0").get();
+                                temp = doc.select("p");
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                            if (temp.size() < 30) {
+                                try {
+                                    doc = Jsoup.connect("https://boxnovel.com/novel/" + novelName + "/chapter-" + page).userAgent("mozilla/17.0").get();
+                                    temp = doc.select("p");
+                                    String errorMessage2 = "Download Complete";
+                                    JOptionPane.showMessageDialog(null, errorMessage2);
+                                    System.exit(1);
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+
+                        } catch (IOException e) {
                         e.printStackTrace();
+
+                        k++;
+
+                    }
                     }
 
-                    k++;
+                    if (test.size() <= 2) {
+                        pageCheck++;
+
+
+                        for (Element novelText : temp) {
+
+                            pdfBoxnovel.novel.add(novelText.getElementsByTag("p").first().text().replace("&lt;", "<").replace("&gt;", ">"));
+                            pdfBoxnovel.allChapters.add(pdfBoxnovel.novel.get(i));
+                            i++;
+                            if (novelText.toString().contains(check)) {
+                                pdfBoxnovel.allChapters.add("\n\n\n\n\n");
+                                break;
+                            }
+                        }
+
+                        System.out.println("Chapter " + page);
+
+                    } else if (test.size() == 0) {
+                        for (Element novelText : temp) {
+
+                            pdfBoxnovel.novel.add(novelText.getElementsByTag("p").first().text().replace("&lt;", "<").replace("&gt;", ">"));
+                            pdfBoxnovel.allChapters.add(pdfBoxnovel.novel.get(i));
+                            i++;
+                            if (novelText.toString().contains(check)) {
+                                pdfBoxnovel.allChapters.add("\n\n\n\n\n");
+
+                            }
+                        }
+
+                        break;
+
+                    }
+
+                    createPDF.Test(novelName);
 
                 }
+                String errorMessage2 = "Download Complete";
+                JOptionPane.showMessageDialog(null, errorMessage2);
+                System.exit(1);
+
+            }
+
+            if (novelWebsite.equals(checkWebsite1)) {
 
 
+                for (page = 1; page <= pageCheck; page++) {
 
-                Elements test = doc.getElementsByClass("nav-next");
+                    int i = 0;
+                    String check = "BoxNovel";
+
+                    pdfBoxnovel.getNovel().clear();
+
+                    Document doc = null;
+                    try {
+
+                        doc = Jsoup.connect("https://boxnovel.com/novel/" + novelName + "/chapter-" + page).userAgent("mozilla/17.0").get();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        String errorMessage1 = "Could not find novel, try again";
+                        JOptionPane.showMessageDialog(null, errorMessage1);
+                        getNovel();
+                    }
 
 
+                    Elements temp = doc.select("p");
+                    Elements test = doc.getElementsByClass("nav-next");
 
-                if (test.size() != 1) {
-                    pageCheck++;
+                    int k = 1;
 
+                    if (temp.size() < 30 && test.size() != 0) {
 
-                    for (Element novelText : temp) {
+                        try {
+                            doc = Jsoup.connect("https://boxnovel.com/novel/" + novelName + "/chapter-" + page + "_" + k).userAgent("mozilla/17.0").get();
+                            temp = doc.select("p");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
 
-                        pdfBoxnovel.novel.add(novelText.getElementsByTag("p").first().text().replace("&lt;", "<").replace("&gt;", ">"));
-                        pdfBoxnovel.allChapters.add(pdfBoxnovel.novel.get(i));
-                        i++;
-                        if (novelText.toString().contains(check)) {
-                            pdfBoxnovel.allChapters.add("\n\n\n\n\n");
-                            break;
+                        k++;
+
+                    } else if (temp.size() < 30) {
+                        try {
+                            doc = Jsoup.connect("https://boxnovel.com/novel/" + novelName + "/chapter-" + page + "-" + "end").userAgent("mozilla/17.0").get();
+                            temp = doc.select("p");
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
 
-                    System.out.println("Chapter " + page);
+                    if (test.size() <= 2) {
+                        pageCheck++;
 
-                } else {
-                    pageCheck = 0;
+
+                        for (Element novelText : temp) {
+
+                            pdfBoxnovel.novel.add(novelText.getElementsByTag("p").first().text().replace("&lt;", "<").replace("&gt;", ">"));
+                            pdfBoxnovel.allChapters.add(pdfBoxnovel.novel.get(i));
+                            i++;
+                            if (novelText.toString().contains(check)) {
+                                pdfBoxnovel.allChapters.add("\n\n\n\n\n");
+                                break;
+                            }
+                        }
+
+                        System.out.println("Chapter " + page);
+
+                    } else if (test.size() == 0) {
+                        for (Element novelText : temp) {
+
+                            pdfBoxnovel.novel.add(novelText.getElementsByTag("p").first().text().replace("&lt;", "<").replace("&gt;", ">"));
+                            pdfBoxnovel.allChapters.add(pdfBoxnovel.novel.get(i));
+                            i++;
+                            if (novelText.toString().contains(check)) {
+                                pdfBoxnovel.allChapters.add("\n\n\n\n\n");
+
+                            }
+                        }
+
+                        break;
+
+                    }
+
+                    if (temp.size() > 30) {
+                        createPDF.Test(novelName);
+                    } else {
+                        break;
+                    }
                 }
+                String errorMessage2 = "Download Complete";
+                JOptionPane.showMessageDialog(null, errorMessage2);
+                System.exit(1);
 
-                createPDF.Test(novelName);
             }
-        } else {
-            getNovel();
-            String errorMessage2 = "The chosen website is not supported yet";
-            JOptionPane.showMessageDialog(null, errorMessage2);
         }
     }
-}
+
